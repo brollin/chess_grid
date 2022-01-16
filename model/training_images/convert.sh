56fc0e095bcc3d5a2eb0d4a54a31405b4b2f3ea2
Ben Rollin <ben.rollin@clever.com>
2022-01-16 00:24:48 -0800

    resize training images

--- model/training_images/convert.sh
+++ model/training_images/convert.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
 
-find . -type f -name '??.svg' | awk -F / '{print $0 " " $3 " " $4}' | while read filepath chessset filename; do (cp "${filepath}" "${filename/.*}/${chessset}.svg" && cd "${filename/.*}" && qlmanage -t -s 1000 -o . "${chessset}.svg"); done
+find . -type f -name '??.svg' | awk -F / '{print $0 " " $3 " " $4}' | while read filepath chessset filename; do (cp "${filepath}" "${filename/.*}/${chessset}.svg" && cd "${filename/.*}" && qlmanage -t -s 100 -o . "${chessset}.svg" && rm "${chessset}.svg"); done
 
+find . -name "*.svg.png" | while read filename; do mv $filename ${filename/.svg}; done
